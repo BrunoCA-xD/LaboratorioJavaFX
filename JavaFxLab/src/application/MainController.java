f3047de0d8d182005b7fa3147155cc399c42f3b2
BrunoCA-xD <39098765+BrunoCA-xD@users.noreply.github.com>
2018-09-10 00:05:09 -0300

    DatePicker and showPassword (with icon)
    DatePicker and showPassword (with icon)

--- JavaFxLab/src/application/MainController.java
+++ JavaFxLab/src/application/MainController.java
@@ -1,12 +1,21 @@
 package application;
 
+
 import javafx.beans.property.BooleanProperty;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleButton;
 import javafx.scene.control.skin.TextFieldSkin;
+import javafx.event.EventHandler;
+import javafx.scene.Cursor;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.beans.value.ObservableValue;
+import javafx.beans.value.ChangeListener;
 
 public class MainController {
 
@@ -22,8 +31,8 @@ public class MainController {
 	@FXML
 	public void initialize() {
 		tx = new textSkin(txtPass);
-		tx.setDoMask(true);
 		txtPass.setSkin(tx);
+		
 		
 		
 	}
@@ -39,14 +48,75 @@ class textSkin extends TextFieldSkin {
 
 	public textSkin(TextField control) {
 		super(control);
+		if (control instanceof PasswordField) {
+			control.getStylesheets().add(textSkin.class.getResource("style.css").toExternalForm());
+			control.getStyleClass().addAll("txt","withicon", "showing");
+			
+
+			control.setOnMousePressed(new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent event) {
+                    double vIconsLeftMargin = control.getWidth() - 20;
+                    final boolean vDoMask = event.getX() <= vIconsLeftMargin;
+                    setDoMask(vDoMask);
+                    setIcon(vDoMask);
+                }
+            });
+
+			control.setOnMouseReleased(new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent event) {
+                    if (!_doMask) {
+                        setDoMask(true);
+                        setIcon(true);
+                    }
+                }
+            });
+			control.focusedProperty().addListener(new ChangeListener<Boolean>() {
+				public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
+					if (newValue) {
+						setIcon(false);
+					}
+					if (!newValue) {
+						setIcon(true);
+					}
+
+				}
+			});
+
+
+			control.setOnMouseMoved((MouseEvent event) -> {
+                double vIconsLeftMargin = control.getWidth() - 20;
+                if (event.getX() > vIconsLeftMargin) {
+                	control.setCursor(Cursor.HAND);
+                } else {
+                	control.setCursor(Cursor.TEXT);
+                }
+            });
+
+			control.setOnMouseExited((MouseEvent event) -> {
+                setDoMask(true);
+                setIcon(true);
+            });
+        }
 		// TODO Auto-generated constructor stub
+	
 	}
+	private void setIcon(boolean pShowing) {
+        TextField textField = this.getSkinnable();
+        textField.getStyleClass().removeAll("showing", "hiding");
+        if (pShowing) {
+            textField.getStyleClass().add("showing");
+        } else {
+            textField.getStyleClass().add("hiding");
+        }
+    }
 
 	public void setDoMask(boolean pValue) {
-
+		TextField textField = this.getSkinnable();
+        textField.getStyleClass().removeAll("showing", "hiding");
 		_doMask = pValue;
 
-		TextField textField = this.getSkinnable();
 
 		String vText = textField.getText();
 		textField.setText(vText);
